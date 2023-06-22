f26e6f737331b5d71f564442001eefb84f888033
badahertz52 <badahertz52@gmail.com>
2023-06-22 21:53:54 +0900

    [Feat] Make function that user can drop down image file

--- src/components/FileContainer.tsx
+++ src/components/FileContainer.tsx
@@ -1,25 +1,37 @@
-import { ChangeEvent, Dispatch, SetStateAction, useRef } from "react";
+import React, {
+  ChangeEvent,
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useRef,
+} from "react";
+import { useDropzone } from "react-dropzone";
 type FileContainerProps = {
   setImgList: Dispatch<SetStateAction<string[] | undefined>>;
 };
 const FileContainer = (props: FileContainerProps) => {
   const inputRef = useRef<HTMLInputElement>(null);
-  const onchangeInput = (event: ChangeEvent) => {
-    const files = (event.target as HTMLInputElement).files;
-    if (files?.[0]) {
-      const reader = new FileReader();
-      reader.readAsDataURL(files[0]);
-      reader.onloadend = (event) => {
-        const result = event.target?.result as string;
-        props.setImgList((prev) => (!prev ? [result] : [result, ...prev]));
-      };
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    if (acceptedFiles[0]) {
+      acceptedFiles.forEach((f) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(f);
+        reader.onloadend = (event) => {
+          const result = event.target?.result as string;
+          props.setImgList((prev) => (!prev ? [result] : [result, ...prev]));
+        };
+      });
     }
-  };
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
   return (
-    <div className="file-container">
-      <input type="file" ref={inputRef} onChange={onchangeInput} />
-      <div className="plus-box" onClick={() => inputRef.current?.click()}>
-        +
+    <div className="file-container" {...getRootProps()}>
+      <div
+        className={`plus-box ${isDragActive ? "on" : ""}`}
+        onClick={() => inputRef.current?.click()}
+      >
+        <input type="file" ref={inputRef} {...getInputProps()} />
+        {isDragActive ? "drop down" : "+"}
       </div>
     </div>
   );
