c6b9b8892e745dd5aa995ccdb7d785c438dd3927
badahertz52 <badahertz52@gmail.com>
2023-06-26 15:44:10 +0900

    [Test] Modify App.test.tsx

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,9 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../components/App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  test("App rendering", () => {
+    render(<App />);
+    expect(screen.getByTestId("gallery-box")).toBeInTheDocument();
+  });
 });
